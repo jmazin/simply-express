db2eacdead3b00fd5769391e6b328deb33413051
Jonathan Mazin <jmazin@brainstation.io>
2023-07-19 12:36:25 -0400

    log port

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
+console.log('this port is', PORT)
+
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my Express App</h1>`);
 });
